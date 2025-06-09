7075ec4ad15a3cf3b3f384602b408d603d07f68d
SUITE <DW1@mail.com>
2025-06-09 16:23:13 -0500

    fix: Wrong dependency was fixed.

--- app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
+++ app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
@@ -6,21 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModelProvider
 import edu.pe.cibertec.financial_track_app.data.DatabaseProvider
-import edu.pe.cibertec.financial_track_app.data.TransaccionRepository
 import edu.pe.cibertec.financial_track_app.ui.theme.Financial_track_appTheme
-import edu.pe.cibertec.financial_track_app.viewmodel.TransaccionViewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            val db = DatabaseProvider.getDatabase(applicationContext)
-            val repository = TransaccionRepository(db.TranasaccionDao())
+        val db = DatabaseProvider.getDatabase(applicationContext)
 
-            val factory = TransaccionViewModel(repository)
+        setContent {
+
             //val viewModel = ViewModelProvider(this,factory)[TransaccionViewModel::class.java]
             Financial_track_appTheme {
                 MainScreen()
