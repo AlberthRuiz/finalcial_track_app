37d5f3a46f19d41341c7df35cd0aa5545820e0fd
SUITE <DW1@mail.com>
2025-06-12 16:25:43 -0500

    fix: Card complet.

--- app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
+++ app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
@@ -1,14 +1,26 @@
 package edu.pe.cibertec.financial_track_app
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,7 +30,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import edu.pe.cibertec.financial_track_app.data.DatabaseProvider
 import edu.pe.cibertec.financial_track_app.data.Transaccion
@@ -39,7 +53,6 @@ class MainActivity : ComponentActivity() {
 
 
         setContent {
-
             Financial_track_appTheme {
                 MainScreen(viewModel)
             }
@@ -68,17 +81,51 @@ fun MainScreen(viewModel: TransaccionViewModel) {
         }
         ) {
         paddingVal ->
-         @androidx.compose.runtime.Composable {
+         @Composable {
             Column (modifier = Modifier.padding(paddingVal).fillMaxSize()
             ) {
-                Text("Hola mundo")
+                LazyColumn {
+                    items(transaccciones) {transaccion  ->
+                        TransaccionItem(
+                            transaccion = transaccion,
+                            onDelete = { viewModel.delete(it)},
+                            onClickAction = { transaccionEditar }
+                        )
+
+                    }
+                }
             }
         }
+
     }
 
+}
 
+@Composable
+fun TransaccionItem(
+    transaccion: Transaccion,
+    onDelete: (Transaccion) -> Unit,
+    onClickAction: (Transaccion) -> Unit
+){
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(8.dp).clickable {
+            onClickAction(transaccion)
+        },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(10.dp),
 
-
-
+        ) {
+            Column () {
+                Text("Descripcion")
+                Text("Tipo")
+                Text("Monto")
+            }
+            IconButton(onClick = {onDelete(transaccion)}) {
+                Icon(Icons.Default.Delete, contentDescription = "Eliminar")
+            }
+    }
+    }
 
 }
