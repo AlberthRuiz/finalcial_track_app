094599018a1926b9d3406718ff83c414744ffbe7
SUITE <DW1@mail.com>
2025-06-12 16:04:27 -0500

    fix: change agp vesion and add main Screen.

--- app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
+++ app/src/main/java/edu/pe/cibertec/financial_track_app/MainActivity.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import edu.pe.cibertec.financial_track_app.data.DatabaseProvider
+import edu.pe.cibertec.financial_track_app.data.Transaccion
 import edu.pe.cibertec.financial_track_app.data.TransaccionRepository
 import edu.pe.cibertec.financial_track_app.ui.theme.Financial_track_appTheme
 import edu.pe.cibertec.financial_track_app.viewmodel.TransaccionViewModel
@@ -27,7 +40,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            //val viewModel = ViewModelProvider(this,factory)[TransaccionViewModel::class.java]
             Financial_track_appTheme {
                 MainScreen(viewModel)
             }
@@ -37,5 +49,36 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: TransaccionViewModel) {
+    val transaccciones  by viewModel.trnasaacciones.collectAsState()
+    var showDialog by remember { mutableStateOf(false) }
+    var transaccionEditar by remember { mutableStateOf<Transaccion?>(null)}
+
+    Scaffold (topBar = {
+        TopAppBar( title =  { Text("APP Trackeo Gastos") })
+
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {showDialog= true}
+
+            ) {
+                Text("+")
+
+            }
+        }
+        ) {
+        paddingVal ->
+         @androidx.compose.runtime.Composable {
+            Column (modifier = Modifier.padding(paddingVal).fillMaxSize()
+            ) {
+                Text("Hola mundo")
+            }
+        }
+    }
+
+
+
+
+
 
 }
